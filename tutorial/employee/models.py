9ca4d60aacc7c2fed0df16a0686488858de1594c
EC2 Default User <ec2-user@ip-10-0-0-136.ap-northeast-2.compute.internal>
2023-11-02 05:40:49 +0000

    employee mv

---
+++ tutorial/employee/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+class Employee(models.Model):
+    name = models.CharField(max_length=64, default='')
+    alias = models.CharField(max_length=64, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = 'employee'
+        ordering = ['-id']
+class EmployeeSalary(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='employee_salary')
+    salary = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = 'employee_salary'
+        ordering = ['-id']
+
+
+# Create your models here.
