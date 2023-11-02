a184dc6233c6377db07481641fc35531046ce7cf
EC2 Default User <ec2-user@ip-172-31-12-123.ap-northeast-2.compute.internal>
2023-11-02 05:45:16 +0000

    feat: employee 프로젝트 추가 및 model 생성

---
+++ tutorial/employee/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=64, default='')
+    alias = models.CharField(max_length=64, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'employee'
+        ordering = ['-id']
+
+class EmployeeSalary(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='employee_salary')
+    salary = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'employee_salary'
+        ordering = ['-id']
+
+
